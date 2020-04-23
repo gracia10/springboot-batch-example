99ce1fdbca9299f4eedcc9895e9c593c4cc505b6
gracia10 <gracia10@daum.net>
2020-04-23 22:14:42 +0900

    step-jpa_multithread

--- src/main/java/com/community/batch/jobs/inactive/InactiveUserJobConfig.java
+++ src/main/java/com/community/batch/jobs/inactive/InactiveUserJobConfig.java
@@ -1,7 +1,10 @@
 package com.community.batch.jobs.inactive;
 
 import java.time.LocalDateTime;
-import java.util.List;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.persistence.EntityManagerFactory;
 
@@ -12,23 +15,22 @@ import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.item.ItemProcessor;
-import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JpaItemWriter;
+import org.springframework.batch.item.database.JpaPagingItemReader;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
 
 import com.community.batch.domain.User;
 import com.community.batch.domain.enums.UserStatus;
 import com.community.batch.jobs.inactive.listener.InactiveChunkListener;
 import com.community.batch.jobs.inactive.listener.InactiveIJobListener;
 import com.community.batch.jobs.inactive.listener.InativeStepListener;
-import com.community.batch.jobs.readers.QueueItemReader;
-import com.community.batch.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 @RequiredArgsConstructor
 @Configuration
 public class InactiveUserJobConfig {
@@ -36,10 +38,13 @@ public class InactiveUserJobConfig {
 	private final static int CHUNK_SIZE = 6;
 	private final JobBuilderFactory jobBuilderFactory;
 	private final StepBuilderFactory stepBuilderFactory;
+	private final EntityManagerFactory entityManagerFactory;
 	
-	private final UserRepository userRepository;
+	@Bean
+	public TaskExecutor taskExecutor() {
+		return new SimpleAsyncTaskExecutor("Batch_Task");
+	}
 	
-	@Primary
 	@Bean
 	public Job inactiveUserJob(InactiveIJobListener inactiveIJobListener,Step inactiveUserStep) {
 		return jobBuilderFactory.get("inactiveUserJob")
@@ -51,29 +56,53 @@ public class InactiveUserJobConfig {
 
 	@Bean
 	@JobScope
-	public Step inactiveUserStep(InactiveChunkListener inactiveChunkListener, InativeStepListener inativeStepListener) {
+	public Step inactiveUserStep(InactiveChunkListener inactiveChunkListener, InativeStepListener inativeStepListener,TaskExecutor taskExecutor) {
 		return stepBuilderFactory.get("inactiveUserStep")
 				.<User, User>chunk(CHUNK_SIZE)
-				.reader(inactiveUserReader())
-				.processor(inactiveUserProcessor())
+				.reader(inactiveUserReader(null))
+				.processor(inactiveUserProcesser())
 				.writer(inactiveUserWriter())
 				.listener(inativeStepListener)
 				.listener(inactiveChunkListener)
+				.taskExecutor(taskExecutor)
+				.throttleLimit(2)
 				.build();
 	}
 
-    @Bean
-    @StepScope
-    public QueueItemReader<User> inactiveUserReader() {
-        List<User> oldUsers = userRepository.findByUpdatedDateBeforeAndStatusEquals(LocalDateTime.now().minusYears(1), UserStatus.ACTIVE);
-        return new QueueItemReader<>(oldUsers);
-    }
+	@Bean
+	@StepScope
+	public JpaPagingItemReader<User> inactiveUserReader(@Value("#{jobParameters[nowDate]}") Date nowDate) {
+		
+		JpaPagingItemReader<User> jpaPagingItemReader = new JpaPagingItemReader<User>() {
+			@Override
+			public int getPage() {
+				return 0;
+			}
+		};
+		
+		jpaPagingItemReader
+		.setQueryString("select u from User as u where u.updatedDate < :updatedDate and u.status = :status");
+		
+		Map<String, Object> map = new HashMap<>();
+		map.put("updatedDate", LocalDateTime.ofInstant(nowDate.toInstant(), ZoneId.systemDefault()).minusYears(1));
+		map.put("status", UserStatus.ACTIVE);
+		
+		jpaPagingItemReader.setParameterValues(map);
+		jpaPagingItemReader.setEntityManagerFactory(entityManagerFactory);
+		jpaPagingItemReader.setPageSize(CHUNK_SIZE);
+		jpaPagingItemReader.setSaveState(false);
+		
+		return jpaPagingItemReader;
+	}
 
-    public ItemProcessor<User, User> inactiveUserProcessor() {
-        return User::setInactive;
-    }
+	private ItemProcessor<User, User> inactiveUserProcesser() {
+		return User::setInactive;
 
-    public ItemWriter<User> inactiveUserWriter() {
-        return ((List<? extends User> users) -> userRepository.saveAll(users));
-    }
+	}
+
+	private JpaItemWriter<User> inactiveUserWriter() {
+		JpaItemWriter<User> jpaItemWriter = new JpaItemWriter<>();
+		jpaItemWriter.setEntityManagerFactory(entityManagerFactory);
+		return jpaItemWriter;
+	}
 }
